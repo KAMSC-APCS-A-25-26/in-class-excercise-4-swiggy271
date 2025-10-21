e2a561142fa23e544b581006b2a45757b2ac1bfb
shuler wiegerink <stellarpie225@gmail.com>
2025-10-21 11:21:31 -0400

    done all

--- src/main/java/GuessingGame.java
+++ src/main/java/GuessingGame.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+import java.util.Random;
 public class GuessingGame {
     public static void main(String[] args) {
         // TODO: Prompt the user with a welcome message
@@ -7,5 +7,32 @@ public class GuessingGame {
         // TODO: Use a do...while loop to read guesses until correct
         // TODO: Print "Too low" / "Too high" / "You got it!" accordingly
         // TODO: Use Scanner for input
+
+        int input;
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+        int ans = rand.nextInt(100) + 1;
+        System.out.print("Welcome to the Guessing Game!\nI'm thinking of a number between 1 and 100.");
+
+        System.out.print("\n\n" + ans);
+
+        do {
+            System.out.println("Enter your Guess: ");
+            input = sc.nextInt();
+            if (input > ans)
+            {
+                System.out.println("Too high! Try again.");
+            }
+            else if (input < ans)
+            {
+                System.out.println("Too low! Try again.");
+            }
+            else
+            {
+                System.out.println("You got it!");
+            }
+        } while(input != ans);
+
+
     }
 }
--- src/main/java/ShoppingCart.java
+++ src/main/java/ShoppingCart.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+import java.text.DecimalFormat;
 public class ShoppingCart {
     public static void main(String[] args) {
         // TODO: Ask the user how many items they are purchasing
@@ -7,5 +7,21 @@ public class ShoppingCart {
         // TODO: Accumulate the total cost
         // TODO: Print the final summary line with item count and total
         // TODO: Use Scanner for input
+
+        int numItems;
+        double input, priceTotal;
+        Scanner sc = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.00");
+        priceTotal = 0;
+        System.out.print("How many items are you purchasing? ");
+        numItems = sc.nextInt();
+
+        for(int i = 0; i < numItems; i++)
+        {
+            System.out.print("\nPrice of item" + i + "? ");
+            priceTotal += sc.nextDouble();
+        }
+
+        System.out.print("Your cart has " + numItems + " with a total cost of $" + priceTotal);
     }
 }
